a400531440484419b7534cef74ba85b0bb0540b2
leonardo.palmeiro@dominion-global.com <P4lm3ir0>
2021-02-03 07:50:12 +0100

    changed better performance

--- src/com/palmeiro/Main.java
+++ src/com/palmeiro/Main.java
@@ -16,9 +16,9 @@ public class Main {
 
     private static final String URL_BASE = "https://www.bilbaokirolak.com/";
     private static final String URL_LOGIN = URL_BASE + "bkonline2/doLogin.jsp";
-    private static final String URL_RESERVATION = URL_BASE + "bkonline2/reservas/reservar_horas.jsp?codCom=11&codAct=26&numAut=&fechaReserva=05/02/2021&bkts=1612303550121";
+    private static final String URL_RESERVATION = URL_BASE + "bkonline2/reservas/reservar_pago_finalizado.jsp?";
     private static final String BKTS_PAOLA = URL_BASE + "1612303550121";
-     bkts
+
     public static void main(String[] args) {
 
 
@@ -55,26 +55,66 @@ public class Main {
         //List<WebElement> allElements = driver.findElements(By.xpath("/html/body/div[1]/div[15]/div[3]/div[1]/ul/li"));
         for (int i = 0; i < liTabgs.size(); i++)
         {
-            List<WebElement> anchors = liTabgs.get(i).findElements(By.tagName("a"));
-            for (WebElement element : anchors) {
+            System.out.println(liTabgs.get(i).getTagName());
+
+            try {
+                WebElement anchors = liTabgs.get(i).findElement(By.tagName("a"));
+                if(!anchors.getAttribute("href").contains("#")) {
+                    String[] getUrlText = anchors.getAttribute("href").split("\\?");
+
+                    //call reservation page
+                    driver.get(anchors.getAttribute("href"));
+                    // press button ok
+                    WebElement reservationButton = driver.findElement(By.xpath("/html/body/div[1]/div[15]/div[2]/form/div[2]/div/button"));
+                    reservationButton.click();
+                    // reservation confirmation
+                    WebElement reservationConfirmationButton = driver.findElement(By.xpath("/html/body/div[1]/div[15]/div[2]/form/div/div/a[2]"));
+                    reservationConfirmationButton.click();
+                    // modal confirmation
+                    WebElement reservationModalConfirmationButton = driver.findElement(By.xpath("/html/body/div[1]/div[32]/div/div/div/a[2]"));
+                    reservationModalConfirmationButton.click();
+
+                    //split
+                    String[] hourText = liTabgs.get(i).getText().split("-");
+                    //if(hourText[0].trim().equals("18:00")){
+                        //TODO copiar o text do link e llamar el reservar pago com as informacioes de usuario.
+                    //    System.out.println("has hour!!");
+                    }
+            }catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            /*for (WebElement element : anchors) {
                 if(!element.getAttribute("href").contains("#")) {
-                    System.out.println("Encontrou" + element.getText());
+                    String[] getUrlText = element.getAttribute("href").split("\\?");
+
+                    //call reservation page
+                    driver.get(element.getAttribute("href"));
+                    // press button ok
+                    WebElement reservationButton = driver.findElement(By.xpath("/html/body/div[1]/div[15]/div[2]/form/div[2]/div/button"));
+                    reservationButton.click();
+                    // reservation confirmation
+                    WebElement reservationConfirmationButton = driver.findElement(By.xpath("/html/body/div[1]/div[15]/div[2]/form/div/div/a[2]"));
+                    reservationConfirmationButton.click();
+                    // modal confirmation
+                    WebElement reservationModalConfirmationButton = driver.findElement(By.xpath("/html/body/div[1]/div[32]/div/div/div/a[2]"));
+                    reservationModalConfirmationButton.click();
+
                     //split
                     String[] hourText = liTabgs.get(i).getText().split("-");
                     if(hourText[0].trim().equals("18:00")){
                         //TODO copiar o text do link e llamar el reservar pago com as informacioes de usuario.
                         System.out.println("has hour!!");
                     }
-                }
-            }
-            System.out.println(liTabgs.get(i).getText());
-        }
-        driver.get("https://www.bilbaokirolak.com/bkonline2/reservas/reservar_pago.jsp?codCom=11&codAct=26&numAut=&fechaReserva=05/02/2021&horaReserva=00011A00005;0;0;18:00&textoHora=18:00-18:59&numSuplementos=1&bkts=1612298148880");
-        WebElement reservationButton = driver.findElement(By.xpath("/html/body/div[1]/div[15]/div[2]/form/div/div/a[2]"));
-        reservationButton.click();
-        WebElement saveReservationButton = driver.findElement(By.xpath(" /html/body/div[1]/div[32]/div/div/div/a[2]"));
-        saveReservationButton.click();
 
+                }*/
+            System.out.println(liTabgs.get(i).getText());
+
+        }
         driver.close();
     }
+
+
+
+
 }
